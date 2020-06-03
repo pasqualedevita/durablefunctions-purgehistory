3de1ea48e215a83258f05fdfa7a92903ced83203
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-06-03 23:27:36 +0200

    fix uppercase

--- configure-manual.sh
+++ configure-manual.sh
@@ -2,27 +2,18 @@
 
 CONF_FILE=$1
 
-echo ${STORAGE_ACCOUNT}
-echo ${TASK_HUB}
-echo ${NO_DATE_FILTER}
-echo ${DAYS_BEFORE}
-echo ${NO_STATUS_FILTER}
-echo ${LIST_STATUS}
-
 echo "STORAGE_ACCOUNT=""'${STORAGE_ACCOUNT}'" > ${CONF_FILE}
 
 echo "TASK_HUB=""'${TASK_HUB}'" >> ${CONF_FILE}
 
-if [ "${NO_DATE_FILTER}" = true ]; then
+if [ "${NO_DATE_FILTER^^}" = TRUE ]; then
   echo "DAYS_BEFORE=""'+1'" >> ${CONF_FILE}
 else
   echo "DAYS_BEFORE=""'${DAYS_BEFORE}'" >> ${CONF_FILE}
 fi
 
-if [ "${NO_STATUS_FILTER}" = true ]; then
+if [ "${NO_STATUS_FILTER^^}" = TRUE ]; then
   echo "LIST_STATUS='completed terminated canceled failed'" >> ${CONF_FILE}
 else
   echo "LIST_STATUS=""'${LIST_STATUS}'" >> ${CONF_FILE}
 fi
-
-cat ${CONF_FILE}
