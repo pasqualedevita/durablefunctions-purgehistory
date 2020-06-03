7610523c927b4a778d5ac016af5a76e5f5ed1e05
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-06-03 17:18:56 +0200

    modular script

--- purge-history.sh
+++ purge-history.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
 
-CONF_DIR="df-conf"
+CONF_FILE=$1
+
+if [ ! -f "${CONF_FILE}" ]; then
+  echo "--- ERROR --- ${CONF_FILE} does not exist"
+  exit 1
+fi
+
+source ${CONF_FILE}
+
 DATE_BEFORE=$(date -I -d "-${DAYS_BEFORE} days")
 echo "--- INFO --- DATE BEFORE: "$DATE_BEFORE
 
-for entry in "${CONF_DIR}"/*
-do
+echo "--- INFO --- START PURGE HISTORY STORAGE ACCOUNT: "${STORAGE_ACCOUNT}
+  
+cd dummy-project
+ls -la
+# create empty local.settings.json
+touch local.settings.json
 
-  source $entry
+# Add a local app setting using the value from an Azure Storage account. Requires Azure login.
+func azure storage fetch-connection-string ${STORAGE_ACCOUNT}
 
-  echo "--- INFO --- START PURGE HISTORY STORAGE ACCOUNT: " ${STORAGE_ACCOUNT}
+# Purge orchestration instance state, history, and blob storage for orchestrations older than the specified threshold time.
+# func durable purge-history \
+func durable get-instances \
+  --connection-string-setting ${STORAGE_ACCOUNT}"_STORAGE" \
+  --task-hub-name ${TASK_HUB} \
+  --created-before ${DATE_BEFORE} \
+  --runtime-status ${LIST_STATUS}
 
-  cd dummy-project
-  # create empty local.settings.json
-  touch local.settings.json
+echo "--- INFO --- END PURGE HISTORY STORAGE ACCOUNT: "${STORAGE_ACCOUNT}
 
-  # Add a local app setting using the value from an Azure Storage account. Requires Azure login.
-  func azure storage fetch-connection-string ${STORAGE_ACCOUNT}
-
-  # Purge orchestration instance state, history, and blob storage for orchestrations older than the specified threshold time.
-  # func durable purge-history \
-  func durable get-instances \
-    --connection-string-setting ${STORAGE_ACCOUNT}"_STORAGE" \
-    --task-hub-name ${TASK_HUB} \
-    --created-before ${DATE_BEFORE} \
-    --runtime-status ${LIST_STATUS}
-
-  echo "--- INFO --- END PURGE HISTORY STORAGE ACCOUNT: " ${STORAGE_ACCOUNT}
-
-  # remove local.settings.json
-  rm local.settings.json
-  cd ..
-
-done
+# remove local.settings.json
+rm local.settings.json
