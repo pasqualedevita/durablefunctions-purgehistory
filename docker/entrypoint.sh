1ba5d6e01cd50468acb04f9eff6741e6bfb8b17c
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-06-17 10:50:46 +0200

    fixed POLICY_FILE set unset

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-az account list --output table
-
-POLICY_FILE=$1
-
-if [ -z ${POLICY_FILE+x} ]; then
-  echo "set"${POLICY_FILE}
-  bash /usr/src/app/src/purge-history.sh /usr/src/app/policies/${POLICY_FILE}
-else
-  echo "notset"
+if [[ -z ${POLICY_FILE} ]];
+then
   for policy in /usr/src/app/policies/*.env
   do
     [[ ! -e ${policy} ]] && continue  # continue, if file does not exist
     echo ${policy}
     bash /usr/src/app/src/purge-history.sh ${policy}
   done
+else
+  bash /usr/src/app/src/purge-history.sh /usr/src/app/policies/${POLICY_FILE}
 fi
